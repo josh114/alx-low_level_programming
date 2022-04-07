86e41f89050bfef041a6213418de51d1eb2c1dca
josh114 <freejosh02@gmail.com>
2022-04-07 15:44:36 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-/**
+/*
  * File: 4-puts.c
  * Auth: Joshua Kenedinum
  */
 #include <stdio.h>
 /**
- * main prints programming is like building a multilingual
- *             puzzle, followed by a new line.
- * Return: Always 0
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
