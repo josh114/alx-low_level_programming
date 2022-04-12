134e9cee3b42e1a1204c04feb7e3c1ba6f9d7247
josh114 <freejosh02@gmail.com>
2022-04-12 11:51:30 +0000

    complete project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Joshua Kenedinum
+ *
+ */
+#include <stdio.h>
+/**
+ * main - Prints "_puthcar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+printf("_putchar\n");
+return 0;
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: JOshua Kenedinum
+ */
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
