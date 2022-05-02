4f80383817591d084e25aaee8a1de564e5a95a13
josh114 <freejosh02@gmail.com>
2022-05-02 20:26:42 +0100

    task update

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,34 @@
+/*
+ * File: 3-strspn.c
+ * Auth: Joshua Kenedinum
+ */
+#include "main.h"
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ *         consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result++;
+				break;
+			}
+		}
+		if (s[j] == '\0')
+			return (result);
+	}
+	return (result);
+}
