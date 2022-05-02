c460515bd01f9931af77259bc9a37a5c30d79c17
josh114 <freejosh02@gmail.com>
2022-05-02 21:09:53 +0000

    task complete

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Joshua Kenedinum
+ */
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+UNUSED(argc);
+printf("%s\n", argv[0]);
+return (0);
+}
