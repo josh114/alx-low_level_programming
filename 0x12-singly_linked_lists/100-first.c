ed5ab2feaadc53830d43e2dca422db16e3ed9a3e
josh114 <freejosh02@gmail.com>
2022-05-25 04:39:18 +0000

    task update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+/*
+ * File: 100-first.c
+ * Auth: Joshua kenedinum
+ */
+
+#include <stdio.h>
+
+void __attribute__((constructor)) first(void);
+
+/**
+ * first - Prints a string before the
+ *        main function is executed.
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
