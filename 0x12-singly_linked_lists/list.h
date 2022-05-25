9acb8f45e3ad28398f85add6807f5560f5b93941
josh114 <freejosh02@gmail.com>
2022-05-25 04:20:05 +0000

    update task

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -26,7 +26,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *nw);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
