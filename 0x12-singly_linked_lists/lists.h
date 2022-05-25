6cdd5fdd8b0d5bc4bb3b6a498bbd3c0f4f702f98
josh114 <freejosh02@gmail.com>
2022-05-25 04:23:31 +0000

    new update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 /*
  * File: lists.h
- * Auth: Joshua kenedinum
+ * Auth: Joshua keendinum
  * Desc: Header file containing prototypes and definitions for all functions
  *       and types written in the 0x11-singly_linked_lists directory.
  */
@@ -26,10 +26,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *nw);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H */
+#endif
