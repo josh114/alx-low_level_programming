0465b05395502bbed492280c7e3e7dd846226ef0
josh114 <freejosh02@gmail.com>
2023-05-25 13:07:41 +0100

    fixed 0 to bitwise

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,23 +14,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int x = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while (*b)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*b == '1')
+			x = (x << 1) | 1;
+		else if (*b == '0')
+			x <<= 1;
+		else
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		b++;
 	}
-
-	return (num);
+	return (x);
 }
